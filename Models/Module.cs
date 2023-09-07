f9489d5e7f7a7bd2cb31f3734e0e4c544cf3110b
x0-3 <steph6362@gmail.com>
2023-09-07 03:41:10 +0200

    models id changed

--- Models/Module.cs
+++ Models/Module.cs
@@ -1,8 +1,12 @@
-﻿namespace Session.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Session.Models
 {
     public class Module
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Name { get; set; }
         public int CategoryId { get; set; }
